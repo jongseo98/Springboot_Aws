10dd209567ebe1ccb79e4a99feee3850d9fb10ee
20171119 <jspark98@unist.ac.kr>
2021-08-18 21:29:39 +0900

    success CRUD

---
+++ src/main/java/com/jspark98/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.jspark98.springboot.web.dto;
+
+import com.jspark98.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
